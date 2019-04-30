56938f9a157ce7cf4318e27555523533d101e99b
zw <845603843@qq.com>
2019-04-30 16:54:53 +0800

    个人中心-我的评论接口

--- src/App.vue
+++ src/App.vue
@@ -78,13 +78,13 @@ export default {
     // this.$router.push("/personal/index");
 
     // 品牌商城
-    this.$router.push("/brand/index");
+    // this.$router.push("/brand/index");
 
     // 评价
     // this.$router.push("/personal/order/comment/index");
 
     // 我的评论
-    // this.$router.push("/personal/help/feedback");
+    this.$router.push("/personal/help/feedback");
 
     // 我的购买
     // this.$router.push("/personal/remain/detail");
