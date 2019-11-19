1aba29edb7a67455540fb7f81122ec2bef19fecf
zhuboyuC <897419721@qq.com>
2019-11-19 13:43:29 +0800

    fix

---
+++ src/pages/library/detail.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="ebookshelfPage">
+    <van-list
+      v-model="commentLoading"
+      :finished="commentFinished"
+      finished-text="没有更多了"
+      @load="commentLoad"
+    >
+      <div>
+        <van-row gutter="20" type="flex" class="booklist">
+          <van-col span="8" v-for="(item,index) in shelfList" :key="index">
+            <div class="ratiobox" @click="toDetail(item,index)">
+              <div class="bookImg" v-lazy:background-image="item.pic"></div>
+              <div class="tags" v-if="item.is_try == 1">试读</div>
+              <div class="tags" v-if="item.schedule == 0 && item.is_try == 0">新加入</div>
+            </div>
+            <div class="title">{{ item.title }}</div>
+            <div class="schedule" v-if="item.schedule">{{ item.schedule }}%</div>
+          </van-col>
+        </van-row>
+      </div>
+    </van-list>
+    <EazyNav type="brand"></EazyNav>
+  </div>
+</template>
+
+<style src="@/style/scss/pages/ebook/shelf.scss" scoped lang="scss"></style>
+
+<script>
+import { EBOOK_SHELF_GETS } from "../../apis/ebook.js";
+export default {
+  data() {
+    return {
+      page: 1,
+      shelfList: [],
+      // 分页
+      commentLoading: false,
+      commentFinished: false,
+      loop: null
+    };
+  },
+  mounted() {
+    // this.getList();
+  },
+  methods: {
+    commentLoad() {
+      this.getList();
+    },
+    async getList() {
+      var tStamp = this.$getTimeStamp();
+      let data = {
+        timestamp: tStamp,
+        version: "1.0",
+        page: this.page
+      };
+      data.sign = this.$getSign(data);
+      let res = await EBOOK_SHELF_GETS(data);
+      if (res.hasOwnProperty("response_code")) {
+        // console.log(res);
+        // 异步更新数据
+        var result = res.response_data.result;
+        setTimeout(() => {
+          for (let i = 0; i < result.length; i++) {
+            this.shelfList.push(result[i]);
+          }
+          // 加载状态结束
+          this.commentLoading = false;
+          this.page++;
+
+          // 数据全部加载完成
+          if (this.page > res.response_data.total_page) {
+            this.commentFinished = true;
+            this.page = 1;
+          }
+        }, 600);
+      } else {
+        this.$toast(res.error_message);
+      }
+    },
+    toDetail(item, index) {
+      this.$router.push({
+        name: "ebookdetail",
+        query: {
+          goods_id: item.goods_id
+        }
+      });
+    }
+  }
+};
+</script>
---
+++ src/pages/library/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="libraryRes">
+    <div class="content">
+      <div class="ratiobox">
+        <div class="bookImg" v-lazy:background-image="http://file.mhuoba.com/shop/5/100023/picture/ebook/20190910/18/20190910184916119.jpg"></div>
+      </div>
+      <div class="right">
+        <div class="text">定制标签，定制标签，定制标签，定制标签，定制标签定制标签定制标签定制标签定制标签定制标签定制标签</div>
+        <div class="author">张嘉佳张嘉佳张嘉佳张嘉佳张嘉佳张嘉佳张嘉佳</div>
+        <div class="terrace">
+          <div class="name">湖南文艺知识平台</div>
+          <div class="oval">
+            <div class="bookImg" v-lazy:background-image="http://file.mhuoba.com/shop/6/100024/picture/pages/20191025/16/20191025160738798.png"></div>
+          </div>
+          <div class="oval" style="right: 33px;">
+            <div class="bookImg" v-lazy:background-image="http://file.mhuoba.com/shop/6/100024/picture/pages/20191025/16/20191025160738798.png"></div>
+          </div>
+          <div class="oval" style="right: 46px;">
+            <div class="bookImg" v-lazy:background-image="http://file.mhuoba.com/shop/6/100024/picture/pages/20191025/16/20191025160738798.png"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="@/style/scss/pages/library/resource" scoped lang="scss"></style>
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    mounted () {
+
+    },
+    methods: {
+
+    }
+  }
+</script>
