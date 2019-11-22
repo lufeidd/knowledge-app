87d0a055f2de9afdf838b2db0a4024ae9385bba3
nancyzeng <931734503@qq.com>
2019-11-22 18:48:08 +0800

    测试

--- config/index.js
+++ config/index.js
@@ -15,11 +15,7 @@ module.exports = {
     proxyTable: {
       '/apis': {
         // 测试环境
-<<<<<<< HEAD
-        target: 'http://frontapi.huoba.dev.zby',  //设置你调用的接口域名和端口号 别忘了加http
-=======
-        target: 'http://frontapi.huoba.dev.zw',  //设置你调用的接口域名和端口号 别忘了加http
->>>>>>> c9fc7c85a7692cf736c58f34d7f8cdcce0eed818
+        target: 'http://frontapi.huoba.dev.zzy',  //设置你调用的接口域名和端口号 别忘了加http
         changeOrigin: true, // 是否跨域
         pathRewrite: {
           '^/apis': '/', // 需要rewrite重写的
@@ -32,11 +28,7 @@ module.exports = {
     // host: '198.210.12.253',
     // host: 'localhost', // can be overwritten by process.env.HOST
 
-<<<<<<< HEAD
-    host: 'wap.huoba.dev.zby', // can be overwritten by process.env.HOST
-=======
-    host: 'wap.huoba.dev.zw', // can be overwritten by process.env.HOST
->>>>>>> c9fc7c85a7692cf736c58f34d7f8cdcce0eed818
+    host: 'wap.huoba.dev.zzy', // can be overwritten by process.env.HOST
     port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
 
     autoOpenBrowser: true,
