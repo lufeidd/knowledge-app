3cdbbba2e93356b92cbd56447eff852cc1651651
zhuboyuC <897419721@qq.com>
2019-11-22 18:06:58 +0800

    文件包、图书资源

---
+++ src/apis/bookresource.js
@@ -0,0 +1,12 @@
+/*
+ * author: 朱博宇
+ * date:2019/10/30
+ */
+
+import req from './http.js'
+
+// 图书资源接口
+export const RESOURCE_GET = query => req('post', '/goods/book/resource/get', query);
+
+// 文件包邮件发送接口
+export const FILEPACKAGE_SEND = query => req('post', '/goods/filepackage/send', query);
