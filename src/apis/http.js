e6430b08399cb92cceaff083b0fe86158a4c2a95
nancyzeng <931734503@qq.com>
2019-12-10 11:05:16 +0800

    优化

--- src/apis/http.js
+++ src/apis/http.js
@@ -32,8 +32,12 @@ instance.interceptors.request.use(function (config) {
         if (localStorage.getItem('routerLink').indexOf('/personal/remain/account') != -1 || localStorage.getItem('routerLink').indexOf('/pay/account') != -1 || localStorage.getItem('routerLink').indexOf('/pay/index') != -1 || localStorage.getItem('routerLink').indexOf('/library/detail') != -1) {
             window.location.href = window.location.href.split('#')[0] + '#/404?msg=请在微信端打开~';
         }
-        // 
+        // 引导app端打开
+        if(1) {
+            window.location.href = window.location.href.split('#')[0] + '#/404?msg=请在app端打开~';
+        }
     }
+
     return config;
 }, function (error) {
     // 对请求错误做些什么
