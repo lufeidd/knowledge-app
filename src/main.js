41a2d3fbead850b4596628796efe1c31c010e42c
liruifeng <1361320179@qq.com>
2019-12-11 16:25:05 +0800

    lrf submit

--- src/main.js
+++ src/main.js
@@ -64,11 +64,11 @@ import {
   Field, Toast, Button, Checkbox, CheckboxGroup, Row, Col, Slider, Uploader,
   Cell, CellGroup,
   Icon, Popup, DatetimePicker, SwipeCell, Dialog,
-  Actionsheet,
+  // Actionsheet,
   AddressEdit, Lazyload, SwitchCell, Search, Tag, Circle,
   Tab, Tabs,
   GoodsAction,
-  GoodsActionBigBtn, GoodsActionMiniBtn,
+  // GoodsActionBigBtn, GoodsActionMiniBtn,
   Step, Steps,
   List, Stepper, SubmitBar,
   Swipe, SwipeItem,
@@ -92,7 +92,7 @@ Vue.use(Popup)
 Vue.use(DatetimePicker)
 Vue.use(SwipeCell)
 Vue.use(Dialog)
-Vue.use(Actionsheet)
+// Vue.use(Actionsheet)
 Vue.use(AddressEdit)
 Vue.use(Lazyload)
 Vue.use(SwitchCell)
@@ -101,7 +101,7 @@ Vue.use(Tag)
 Vue.use(Tab).use(Tabs)
 Vue.use(Circle)
 Vue.use(GoodsAction)
-Vue.use(GoodsActionBigBtn).use(GoodsActionMiniBtn)
+// Vue.use(GoodsActionBigBtn).use(GoodsActionMiniBtn)
 Vue.use(Step).use(Steps)
 Vue.use(List)
 Vue.use(Stepper)
@@ -155,7 +155,7 @@ Vue.config.productionTip = false
 2、只能在app端打开的页面
   meta: {
     isAppLogin: true
-  } 
+  }
 
 3、必须登录才能访问的页面
   meta: {
